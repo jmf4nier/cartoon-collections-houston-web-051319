2b476f883ead0c721d4b6ad4c5d2b007b538c49c
Jason Fournier <jmf4nier@hotmail.com>
2019-05-02 16:44:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,8 @@ soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.collect do |cheese|
-    array.include?(cheese)
+    if array.include?(cheese)
+      array.find(cheese)
   end
   
   
