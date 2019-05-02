a20e8320cd3b9a8e070146f693180a1c3215a44b
Jason Fournier <jmf4nier@hotmail.com>
2019-05-02 16:17:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 #summon_captain_planet(planeteer_calls)
 
 short_words = ["puff", "go", "two"]
-assorted_words = ["two", "go", "industrious", "bop"]
+#assorted_words = ["two", "go", "industrious", "bop"]
 
 def long_planeteer_calls(array)
   array.any? do |words|
